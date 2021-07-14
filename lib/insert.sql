e427c6929795f15adc153ea61de774d5422918ea
baezdiazm <baezdiazm@yahoo.com>
2021-07-14 16:57:38 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES 
+(1, "Booboo", 5, "M", "Blue", "Chill", 1),
+(2, "Yogi", 15, "M", "Brown", "Curious", 0),
+(3, "Bambi", 6, "M", "Brown", "Scary", 0),
+(4, "Jessica", 21, "F", "Pink", "Outgoing", 1),
+(5, "Michael", 24, "M", "Yellow", "Careless", 1),
+(6, "Pichael", 24, "M", "Yellow", "Angry", 1),
+(7, "Oscar", 8, "M", "Black", "Hot Headed", 0),
+(8, null, 3, "F", "White", "Relaxed", 1
+);
